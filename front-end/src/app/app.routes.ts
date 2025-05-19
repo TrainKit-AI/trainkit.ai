efffd14f2b28c32150e74c3ed19482aee521d7a5
mohamed-rahmani <mohamed.rahmani@edu.univ-paris13.fr>
2025-05-19 19:26:51 +0200

    added: application structure

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Error404Component } from './core/components/error-404/error-404.component';
+import { ROOT_PATHS } from './core/constants/paths.constants';
+import { HomeComponent } from './features/home/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: ROOT_PATHS.home,
+    component: HomeComponent,
+  },
+  { path: ROOT_PATHS.error404, component: Error404Component },
+  { path: '**', redirectTo: ROOT_PATHS.error404 },
+];
---
+++ front-end/src/app/core/constants/paths.constants.ts
@@ -0,0 +1,11 @@
+export const ROOT_PATHS = {
+  home: '',
+  error404: '404',
+};
+
+export const LEGAL_PATHS = {
+  legal: 'legal/terms',
+  privacy: 'legal/privacy',
+  cookie: 'legal/cookie',
+  product: 'legal/product-terms',
+};
