64fe56eca6c1d87d3c6a58d56681d34fec306b9e
mohamed-rahmani <mohamed.rahmani@edu.univ-paris13.fr>
2025-05-24 22:26:18 +0200

    added: responsive header component

--- front-end/src/app/features/home/components/header/header.component.ts
+++ front-end/src/app/features/home/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import {
   DropdownMenu,
   SERVICES,
@@ -19,6 +19,44 @@ import { SignUpButtonComponent } from '../sign-up-button/sign-up-button.componen
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   Services: DropdownMenu = SERVICES;
+  isMenuOpen = false;
+
+  ngOnInit() {
+    this.updateBodyScroll();
+  }
+
+  ngOnDestroy() {
+    document.body.style.overflow = 'auto';
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    this.updateBodyScroll();
+  }
+
+  private updateBodyScroll() {
+    if (this.isMenuOpen && window.innerWidth <= 777) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth > 777 && this.isMenuOpen) {
+      this.isMenuOpen = false;
+      this.updateBodyScroll();
+    }
+  }
+
+  @HostListener('document:keydown.escape', ['$event'])
+  onEscapeKey(event: KeyboardEvent) {
+    if (this.isMenuOpen) {
+      this.isMenuOpen = false;
+      this.updateBodyScroll();
+    }
+  }
 }
