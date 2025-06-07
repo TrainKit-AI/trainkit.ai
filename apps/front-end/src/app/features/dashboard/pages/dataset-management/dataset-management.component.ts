00a171dc15dada27f16833dba314dd315ec3d17c
mohamed-rahmani <mohamed.rahmani@edu.univ-paris13.fr>
2025-06-07 21:03:37 +0200

    feat(dataset): add the view page for datasets

--- apps/front-end/src/app/features/dashboard/pages/dataset-management/dataset-management.component.ts
+++ apps/front-end/src/app/features/dashboard/pages/dataset-management/dataset-management.component.ts
@@ -1,9 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dataset-management',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dataset-management.component.html',
   styleUrl: './dataset-management.component.css',
 })
-export class DatasetManagementComponent {}
+export class DatasetManagementComponent {
+  datasets = [
+    {
+      name: 'ImageNet',
+      type: 'Image',
+      creationDate: '2021-06-01',
+      numberOfExamples: 1000000,
+    },
+    {
+      name: 'TextCorpus',
+      type: 'Text',
+      creationDate: '2022-03-15',
+      numberOfExamples: 250000,
+    },
+    {
+      name: 'AudioClips',
+      type: 'Audio',
+      creationDate: '2023-01-20',
+      numberOfExamples: 50000,
+    },
+  ];
+
+  addDataset() {
+    console.log('Add dataset button clicked');
+  }
+
+  viewExamples(dataset: any) {
+    console.log('Viewing examples of', dataset.name);
+  }
+
+  addExample(dataset: any) {
+    console.log('Adding example to', dataset.name);
+  }
+
+  editDataset(dataset: any) {
+    console.log('Editing dataset', dataset.name);
+  }
+}
