d51df732807aa34409efd2d35a3d9f7a9815874d
mohamed-rahmani <mohamed.rahmani@edu.univ-paris13.fr>
2025-06-12 22:32:58 +0200

    fix: entrypoint.sh

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -2,30 +2,17 @@
 set -e
 
 echo "DATABASE_USER=$SPRING_DATASOURCE_USERNAME"
-echo "DATABASE_URL=$SPRING_DATASOURCE_URL"
+echo "DATABASE_HOST=$SPRING_DATASOURCE_URL"
 echo "DATABASE_PASSWORD=$SPRING_DATASOURCE_PASSWORD"
 
-# On enlève le préfixe "jdbc:postgresql://"
-URL="${SPRING_DATASOURCE_URL#jdbc:postgresql://}"
-
-# On récupère host:port/dbname
-HOSTPORT_DB="${URL}"
-
-# Extraire host et port
-HOSTPORT="${HOSTPORT_DB%%/*}"  # tout avant le premier /
-HOST="${HOSTPORT%%:*}"         # tout avant ':'
-PORT="${HOSTPORT##*:}"         # tout après ':'
-
-# Extraire dbname
-DB="${HOSTPORT_DB#*/}"         # tout après le premier '/'
-
-# Construire l'URL psql standard
-PSQL_URL="postgresql://${SPRING_DATASOURCE_USERNAME}:${SPRING_DATASOURCE_PASSWORD}@${HOST}:${PORT}/${DB}"
+# Construire l'URL psql à partir des variables Render
+PSQL_URL="${SPRING_DATASOURCE_USERNAME}:${SPRING_DATASOURCE_PASSWORD}@${SPRING_DATASOURCE_URL}:5432/postgres"
 
 echo "=> Test de connexion à la base de données..."
+
 ATTEMPTS=0
 MAX_ATTEMPTS=30
-while ! psql "$PSQL_URL" -c '\l' > /dev/null 2>&1; do
+while ! PGPASSWORD="$SPRING_DATASOURCE_PASSWORD" psql -h "$PSQL_URL" -p "5432" -U "$SPRING_DATASOURCE" -d "postgres" -c '\l' > /dev/null 2>&1; do
   ATTEMPTS=$((ATTEMPTS+1))
   if [ $ATTEMPTS -ge $MAX_ATTEMPTS ]; then
     echo "⚠️ Impossible de se connecter à la base après $MAX_ATTEMPTS essais, arrêt."
